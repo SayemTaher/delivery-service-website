c0464d463b04a9c3ab117a8effa4adedd2396485
SayemTaher <sayemtaher788@gmail.comn>
2024-06-22 14:30:35 +0200

    revised

--- web-client-side/src/Components/LandingPage/Home.jsx
+++ web-client-side/src/Components/LandingPage/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import Banner from "./Banner";
 import Featured from "./Featured";
 import Statistics from "./Statistics";
@@ -7,6 +8,9 @@ import TopDelivery from "./TopDelivery";
 const Home = () => {
     return (
         <div className="flex flex-col">
+            <Helmet>
+                <title>Fast Track | Home</title>
+            </Helmet>
             <Banner></Banner>
             <Featured></Featured>
             <Statistics></Statistics>
