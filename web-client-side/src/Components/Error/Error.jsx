ac971a3c560fb66882dee4efa5feed3a31f89888
SayemTaher <sayemtaher788@gmail.comn>
2024-06-22 14:33:07 +0200

    error boundary added

--- web-client-side/src/Components/Error/Error.jsx
+++ web-client-side/src/Components/Error/Error.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link, useNavigation } from 'react-router-dom';
 
 const Error = () => {
+    const navigation = useNavigation()
     return (
         <section className="bg-white dark:bg-gray-900">
             <div className="container flex items-center justify-center min-h-screen px-6 py-12 mx-auto">
@@ -17,12 +19,12 @@ const Error = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                                 </svg>
-                                <span>Go back</span>
+                                <Link to=''>Go back</Link>
                             </button>
 
-                            <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                           <Link to='/'> <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
                                 Take me home
-                            </button>
+                            </button></Link>
                         </div>
                     </div>
 
